73d5894482ce5af9d9619f88a2e1c6e608dfb5bc
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2024-01-29 08:30:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KalkulackaPHP</title>
+</head>
+<body>
+    <h1>Tohle je kalukacla v php</h1>
+    <form>
+
+
+
+    <?php
+    $txt = "<p style=\"color: blue;\">  Jsme samozvaní \"Vikingove\" ze severu </p>";
+    echo "<h9> AHOJ </h9> <br> $txt! ";
+    var_dump($txt)
+    ?>
+</body>
+</html>
+
